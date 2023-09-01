d25f9c6b790c6375a375ea1c3b8fffa200350402
Alex Norman <alex@x37v.info>
2023-09-01 08:18:16 -0700

    cpp not c++ for treesitter

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "c", "c++", "lua", "rust", "typescript", "toml", "javascript" },
+  ensure_installed = { "c", "cpp", "lua", "rust", "typescript", "toml", "javascript" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
